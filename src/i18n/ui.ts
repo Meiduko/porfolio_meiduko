fb28961679afb1c40b711c3ac0a00875f9f0204e
Borja González <borjago.bgo@gmail.com>
2025-08-19 17:01:20 +0200

    Feat(i18n): support es, en and gl translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,64 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+  gl: 'Galego'
+}
+export const defaultLang = 'es'
+
+export const ui = {
+  es: {
+    'Title.h1': 'Hola, soy Meiduko',
+    'Title.span': '+2 años',
+    'Title.p': 'Estudiando desarrollo web y diseño UI/UX con las herramientas más populares.',
+    "Projects.h3": "Proyectos",
+    "About.p1": "Mi nombre es Borja y soy de Galicia, España. Desde pequeño me ha interesado la tecnología, pero no fue hasta 2022 que decidí estudiar desarrollo web, y dadas las circunstancias, me he visto obligado a aprender de forma autodidacta, con la ayuda de cursos online y divulgadores de tecnología.",
+    "About.p2.1": "Soy un desarrollador junior con experiencia en diversas tecnologías web que disfruta construyendo aplicaciones y herramientas",
+    "About.p2.span": "user-frendly",
+    "About.p2.2": ", eficientes y escalables. Me encanta aprender nuevas tecnologías y mejorar mis habilidades constantemente.",
+    //TODO: Refactor with some span for emphasis (interfaces, visualmente atractivas, intuitivas, buenas prácticas)
+    "About.p3.1": "Me gusta aprender afrontando nuevos retos, buscando una forma",
+    "About.p3.span.1": "creativa y eficiente",
+    "About.p3.2": "de convertir ideas en código funcional y reutilizable, con un importante enfoque en el",
+    "About.p3.span.2": "detalle y la adaptabilidad.",
+    "About.p4": "Trato de crear interfaces que no solo sean visualmente atractivas, sino también intuitivas y fáciles de usar. Me esfuerzo por mantenerme actualizado con las últimas tendencias y mejores prácticas de cada una de las tecnologías que utilizo.",
+    "About.p5.1": "Tengo ",
+    "About.p5.span": "conocimientos ",
+    "About.p5.2": "de: ",
+  },
+  gl: {
+    'Title.h1': 'Ola, son Meiduko',
+    'Title.span': '+2 anos',
+    'Title.p': 'Estudando desenvolvemento web e deseño UI/UX coas ferramentas máis populares',
+    "Projects.h3": "Proxectos",
+    "About.p1": "O meu nome é Borja e son de Galicia, España. Dende pequeno intereseime pola tecnoloxía, pero non foi ata 2022 cando decidín estudar desenvolvemento web e, dadas as circunstancias, vinme obrigado a aprender de forma autodidacta, coa axuda de cursos en liña e divulgadores de tecnoloxía.",
+    "About.p2.1": "Son un desenvolvedor júnior con experiencia en diversas tecnoloxías web que goza construíndo aplicacións e ferramentas",
+    "About.p2.span": "user-frendly",
+    "About.p2.2": ", eficientes e escalables. Encántame aprender novas tecnoloxías e mellorar constantemente as miñas habilidades.",
+    "About.p3.1": "Gústame aprender afrontando novos retos, buscando unha forma",
+    "About.p3.span.1": "creativa e eficiente",
+    "About.p3.2":"de converter ideas en código funcional e reutilizable, cun importante enfoque no",
+    "About.p3.span.2": "detalle e na adaptabilidade.",
+    "About.p4": "Trato de crear interfaces que non só sexan visualmente atractivas, senón tamén intuitivas e fáciles de usar. Esfórzome por manterme ao día coas últimas tendencias e boas prácticas de cada unha das tecnoloxías que utilizo.",
+    "About.p5.1": "Teño ",
+    "About.p5.span": "coñecementos ",
+    "About.p5.2": "de: ",
+  },
+  en: {
+    'Title.h1': "Hi, I'm Meiduko",
+    'Title.span': '+2 years',
+    'Title.p': 'studying front-end web programming with the most popular tools. I strive to tackle any challenge in the most creative and efficient way possible.',
+    "Projects.h3": "Projects",
+    "About.p1": "My name is Borja and I am from Galicia, Spain. I have been interested in technology since I was a child, but it wasn’t until 2022 that I decided to study web development. Given the circumstances, I have had to learn in a self-taught way, with the help of online courses and technology educators.",
+    "About.p2.1": "I am a junior developer with experience in various web technologies who enjoys building",
+    "About.p2.span": "user-friendly",
+    "About.p2.2": ", efficient, and scalable applications and tools. I love learning new technologies and constantly improving my skills.",
+    "About.p3.1": "I like learning by facing new challenges, looking for a",
+    "About.p3.span.1": "creative and efficient",
+    "About.p3.2": "way to turn ideas into functional and reusable code, with a strong focus on",
+    "About.p3.span.2": "detail and adaptability.",
+    "About.p4": "I try to create interfaces that are not only visually appealing but also intuitive and easy to use. I strive to stay up to date with the latest trends and best practices in each of the technologies I use.",
+    "About.p5.1": "With ",
+    "About.p5.span": "knowledge ",
+    "About.p5.2": "of: ",
+  }
+} as const
