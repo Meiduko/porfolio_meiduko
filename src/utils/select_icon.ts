c198dfe35a7bf8aed31a9b12bd76f9724e95e2dc
Borja González <borjago.bgo@gmail.com>
2025-08-27 13:04:45 +0200

    Feat(icons): add SVG icons for Astro, CSS, HTML, JavaScript, React, and Tailwind

--- src/utils/select_icon.ts
+++ src/utils/select_icon.ts
@@ -1,9 +1,9 @@
-import AstroIcon from "@/assets/icons/AstroIcon.astro"
-import CSSIcon from "@/assets/icons/CSSIcon.astro"
-import JavaScriptIcon from "@/assets/icons/JavaScriptIcon.astro"
-import ReactIcon from "@/assets/icons/ReactIcon.astro"
-import TailwindIcon from "@/assets/icons/TailwindIcon.astro"
-import HTMLIcon from "@/assets/icons/HTMLIcon.astro"
+import AstroIcon from "@/assets/icons/tech/AstroIcon.astro"
+import CSSIcon from "@/assets/icons/tech/CSSIcon.astro"
+import JavaScriptIcon from "@/assets/icons/tech/JavaScriptIcon.astro"
+import ReactIcon from "@/assets/icons/tech/ReactIcon.astro"
+import TailwindIcon from "@/assets/icons/tech/TailwindIcon.astro"
+import HTMLIcon from "@/assets/icons/tech/HTMLIcon.astro"
 
 export const TECHENUM = ['Astro', 'React', 'TailwindCSS', 'CSS', 'JavaScript', 'HTML'] as const
 export type techType = (typeof TECHENUM)[number]
